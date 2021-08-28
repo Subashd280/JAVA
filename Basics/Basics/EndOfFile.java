95db124dc4a6064e56143c140bb109001720d242
subashchandran <subashchandran.appstone@gmail.com>
2021-08-28 17:53:33 +0530

    End of file

---
+++ Basics/Basics/EndOfFile.java
@@ -0,0 +1,12 @@
+package Basics;
+import java.util.*;
+import java.io.*;
+
+public class EndOfFile {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        for(int i = 1; sc.hasNext()==true; i++){
+            System.out.println(i+ " "+sc.nextLine());
+        }
+    }
+}
