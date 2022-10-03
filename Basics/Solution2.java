865f490663c2e62dfa997d90f34793c1e9c0d8bd
subashchandran <subashchandran.appstone@gmail.com>
2022-10-04 01:34:07 +0530

    qrcode

--- Basics/Solution2.java
+++ Basics/Solution2.java
@@ -1,3 +1,5 @@
+package Basics;
+
 import java.util.*;
 import java.io.*;
 
@@ -39,9 +41,9 @@ class Solution2{
 }
 
 
- if(x >= (short)-Math.pow(2, 15) && x <=(short) Math.pow(2, 15) - 1)
-        System.out.println("* short");
-        if(x >= (int)-Math.pow(2, 31) && x <=(int) Math.pow(2, 31) - 1)
-            System.out.println("* int");
-        if(x >= (long)-Math.pow(2, 63) && x <=(long) Math.pow(2, 63) - 1)
-            System.out.println("* long");
+// if(x >= (short)-Math.pow(2, 15) && x <=(short) Math.pow(2, 15) - 1)
+//        System.out.println("* short");
+//        if(x >= (int)-Math.pow(2, 31) && x <=(int) Math.pow(2, 31) - 1)
+//            System.out.println("* int");
+//        if(x >= (long)-Math.pow(2, 63) && x <=(long) Math.pow(2, 63) - 1)
+//            System.out.println("* long");
